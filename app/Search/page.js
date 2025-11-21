7281f463afedf3a2052eb9465403db7e7b187e80
Kittimaeve11 <kittima.eve.11@gmail.com>
2025-11-21 16:08:41 +0700

    edit code searchContent

--- app/Search/page.js
+++ app/Search/page.js
@@ -1,23 +1,5 @@
-'use client';
-
 import { Suspense } from 'react';
-import Navbar from "../components/Navbar";
-import TabMenu from "../components/TabMenu";
-import { useLocale } from '../Context/LocaleContext';
-
-function SearchContent() {
-  const { messages } = useLocale();
-
-  return (
-    <div>
-      <Navbar />
-      <TabMenu />
-      <main style={{ padding: '1rem' }}>
-        <h1>{messages.search}</h1>
-      </main>
-    </div>
-  );
-}
+import SearchContent from './SearchContent';
 
 export default function Page() {
   return (
