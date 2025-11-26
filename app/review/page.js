0d0e1786b7d87a7c81ddf461bc9734bb80ccdaba
Kittimaeve11 <kittima.eve.11@gmail.com>
2025-11-26 13:20:45 +0700

    Updated the review page code to add <Suspense>

--- app/review/page.js
+++ app/review/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import ReviewClient from './ReviewClient';
 
 export default function Page() {
-  return <ReviewClient />;
+  return (
+    <Suspense fallback={null}>
+      <ReviewClient />
+    </Suspense>
+  );
 }
